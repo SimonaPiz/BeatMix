d8072747eff4c0e25d10c98551435ef6c2439679
Simona Pizio <simona.pizio@gmail.com>
2023-10-09 11:33:40 +0200

    #1 implementation -#2 Add function toggleDrum in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,48 @@ let snares = new Array(16).fill(false);
 let hiHats = new Array(16).fill(false);
 
 let rideCymbals = new Array(16).fill(false);
+
+/*2.
+una funzione chiamata toggleDrumche accetta due argomenti: una stringa che rappresenta il nome 
+dell'array ( 'kicks', 'snares', 'hiHats'o 'rideCymbals') e un numero di indice. Questa funzione 
+dovrebbe capovolgere il valore booleano nell'array corretto all'indice specificato.
+*/
+const toggleDrum = (arrayName, index) => {
+  // verifichiamo che i valori di index siano validi
+  if(index < 0 || index > 16) {
+    return;
+  };
+
+  // toggle drum
+  switch (arrayName) {
+    case 'kicks':
+      if (kicks[index] === true) {
+        kicks[index] = false;
+      } else {
+        kicks[index] = true;
+      };
+      break;
+    case 'snares':
+      if (snares[index] === true) {
+        snares[index] = false;
+      } else {
+        snares[index] = true;
+      };
+      break;
+    case 'hiHats':
+      if (hiHats[index] === true) {
+        hiHats[index] = false;
+      } else {
+        hiHats[index] = true;
+      };
+      break;
+    case 'rideCymbals':
+      if (rideCymbals[index] === true) {
+        rideCymbals[index] = false;
+      } else {
+        rideCymbals[index] = true;
+      };
+      break;
+    default: return;
+  };
+};
