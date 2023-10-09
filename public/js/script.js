1e1b7919c19cae38316b378c6d3eb6fbf8b767f4
Simona Pizio <simona.pizio@gmail.com>
2023-10-09 11:26:14 +0200

    #1 implementation - Add four variables in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,13 @@
+/*1.
+Quattro variabili per rappresentare gli array di drum pad. Questi array prendono il nome dai 
+tamburi che rappresentano: kicks, snares, hiHats, rideCymbals. Questi array dovrebbero avere 
+tutti una lunghezza 16e essere riempiti con false.
+*/
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = new Array(16).fill(false);
+
+let snares = new Array(16).fill(false);
+
+let hiHats = new Array(16).fill(false);
+
+let rideCymbals = new Array(16).fill(false);
