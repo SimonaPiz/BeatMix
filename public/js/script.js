3ed854692b04c0851fc76894fcb76b61d2a05b60
Simona Pizio <simona.pizio@gmail.com>
2023-10-09 11:55:44 +0200

    #1 implementation -#7 Add function getNeighborPads in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -124,3 +124,53 @@ const invert = (arrayName) => {
     default: return;
   };  
 };
+
+/*BONUS.
+una funzione chiamata getNeighborPads che accetta un parametro x, y e size. 
+Nell'applicazione, questi valori si riferiscono alla griglia del sintetizzatore: x e y indicizzati 
+a zero dalla parte inferiore sinistra della griglia, ed sizeè un numero che rappresenta il numero 
+di righe/colonne nel quadrato. getNeighborPadsdovrebbe restituire un array di neighbor, ciascuno 
+nel formato [xValue, yValue]. I vicini sono i quadrati immediatamente a sinistra, a destra, sopra e 
+sotto una posizione della griglia.
+
+Per lavorare sul bonus con i test, dovrai rimuovere il loro stato in sospeso. 
+Apri test/test.js e modifica la riga che inizia xdescribe('BONUS: getNeighborPads() 
+function'(dovrebbe essere intorno alla riga 360 nel file di test). 
+Se elimini il x(in modo che la riga inizi semplicemente con describe(e salvi il file di test 
+prima di rieseguirlo, i tuoi test bonus saranno ora attivi.
+*/
+const getNeighborPads = (x, y, size) => {
+  if ( x < 0 || y < 0 || x >= size || y >= size) {
+    return [];
+  };
+
+  switch (x) {
+    case 0:
+      if (y === 0) {
+        return [[1, 0], [0, 1]];
+      } else if (y === size-1) {
+        return [[0, y-1], [1, y]];
+      } else {
+        return [[0, y-1], [1, y], [0, y+1]];        
+      }
+      break;
+    case size-1:
+      if (y === 0) {
+        return [[x-1, 0], [x, 1]];
+      } else if (y === size-1) {
+        return [[x, y-1], [x-1, y]];
+      } else {
+        return [[x, y-1], [x-1, y], [x, y+1]];        
+      }
+      break;
+    default:
+      if (y === 0) {
+        return [[x-1, 0], [x, 1], [x+1, 0]];
+      } else if (y === size-1) {
+        return [[x-1, y], [x, y-1], [x+1, y]];
+      } else {
+        return [[x-1, y], [x, y-1], [x+1, y], [x, y+1]];        
+      }
+      break;
+  };
+};
